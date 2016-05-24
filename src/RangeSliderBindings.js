c148cb53fc382705185ef4e1633ee3fbf5ceb180
Michael Rouse <mwrouse@outlook.com>
2016-05-24 16:57:09 -0500

    Added sliderStep binding

--- src/RangeSliderBindings.js
+++ src/RangeSliderBindings.js
@@ -101,3 +101,31 @@ ko.bindingHandlers.sliderMin = {
   } // End update 
   
 };
+
+
+
+/**
+ * The sliderStep custom binding allows another observable to control the step value of a slider
+ */
+ko.bindingHandlers.sliderStep = {
+  // Init, runs on initialization 
+  init: function ( element, valueAccessor, allBindings, viewModel, bindingContext ) {
+    // Setup the initial step value if it is not an observable
+    if ( !ko.isObservable( valueAccessor() ) && ( element instanceof HTMLInputElement ) && ( element.type === "range" ) )
+    {
+      element.step = valueAccessor(); // Not an observable 
+    }
+    
+  }, // End init
+  
+  // Update, runs whenever observables for this binding change (and on initialization)
+  update: function ( element, valueAccessor, allBindings, viewModel, bindingContext ) {
+    // Update function is only meant for observables
+    if ( ko.isObservable( valueAccessor() ) && ( element instanceof HTMLInputElement ) && ( element.type === "range" ) )
+    {
+      // Set new step value 
+      element.step = ko.unwrap( valueAccessor() );
+    }
+  } // End update 
+  
+};
